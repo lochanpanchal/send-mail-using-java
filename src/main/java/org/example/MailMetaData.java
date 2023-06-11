ea1935774cec2ea1db3c905dda01b61e0bfb36ba
lochan panchal <lakerajpanchal98@gmail.com>
2023-06-11 17:50:47 +0530

    learn how to send email using java, app password

---
+++ src/main/java/org/example/MailMetaData.java
@@ -0,0 +1,21 @@
+package org.example;
+
+
+
+public class MailMetaData {
+
+    public static final String HostServer = "smtp.gmail.com";
+    public static final String port = "465";
+
+    public static final String sslProperty = "mail.smtp.ssl.enable";
+
+    public static final String authPerm = "mail.smtp.auth";
+
+    public static final String myUserMail = "lakerajpanchal98@gmail.com";
+
+    public static final String password = "dmokxgszxflccrja"; // app password
+
+    public static final String receiverMail = "jiorange0@gmail.com";
+
+}
+
